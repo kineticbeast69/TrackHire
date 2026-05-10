82e3888c4df105029e61504c272661604b03b092
kineticbeast69 <tiarishubham@gmail.com>
2026-05-10 16:01:02 +0530

    make the dashboard ui

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,15 @@
+import KabanBoard from "@/components/kabanBoard";
+
 export default function DashboardPage() {
-  return "this is an dasboard page.";
+  return (
+    <div className="min-h-screen bg-white">
+      <div className="container mx-auto p-6">
+        <div className="mb-6">
+          <h1 className="text-3xl font-bold text-black">Job Hunt</h1>
+          <p className="text-gray-600">Track your job application</p>
+        </div>
+        <KabanBoard />
+      </div>
+    </div>
+  );
 }
---
+++ components/kabanBoard.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Award, Calendar, CheckCircle2, Mic, XCircle } from "lucide-react";
+
+export default function KabanBoard() {
+  const columnConfig = [
+    {
+      color: "bg-cyan-500",
+      icon: <Calendar className="w-4 h-4" />,
+    },
+    {
+      color: "bg-purple-500",
+      icon: <CheckCircle2 className="h-4 w-4" />,
+    },
+    {
+      color: "bg-green-500",
+      icon: <Mic className="h-4 w-4" />,
+    },
+    {
+      color: "bg-yellow-500",
+      icon: <Award className="h-4 w-4" />,
+    },
+    {
+      color: "bg-red-500",
+      icon: <XCircle className="h-4 w-4" />,
+    },
+  ];
+  return (
+    <>
+      <div>
+        <div></div>
+      </div>
+    </>
+  );
+}
